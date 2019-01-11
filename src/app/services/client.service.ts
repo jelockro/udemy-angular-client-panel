66a9ecd9b9012c89b6972049aa3e7842bcd33bf7
Josh <jlockrow@gmail.com>
2019-01-11 09:40:45 -0600

    adds imports to client.service

--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
