8f30935a21120e2216d9b06043d2397e020a65b3
Josh <jlockrow@gmail.com>
2019-01-11 14:17:39 -0600

    Adds client-deletion functionality

--- src/app/components/client-details/client-details.component.ts
+++ src/app/components/client-details/client-details.component.ts
@@ -43,4 +43,12 @@ export class ClientDetailsComponent implements OnInit {
     this.flashMessage.show('Balance updated', {cssClass: 'alert-success', timeout: 4000});
   }
 
+  onDeleteClick() {
+    if(confirm('Are you sure?')){
+      this.clientService.deleteClient(this.client);
+      this.flashMessage.show('Client removed', {cssClass: 'alert-success', timeout: 4000});
+      this.router.navigate(['/']);
+    }
+  }
+
 }
